5cfe2f7169ecdf814cdd612d1741f301c4aba9ab
minha0817 <minhakk24@gmail.com>
2023-06-23 12:34:56 -0700

    Clean up the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { getDaycare } from "@/api/getDaycare";
+
 export default async function Home() {
+
+  const daycare = await getDaycare();
+
   return (
     <div>
-      <h1>Welcome to Hipapa</h1>
+      <h1>Welcome to HIPAPA</h1>
     </div>
   );
 }
--- src/lib/supabaseClient.tsx
+++ src/lib/supabaseClient.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://gzwroakkfulgmjhijyws.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6d3JvYWtrZnVsZ21qaGlqeXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY2Nzk0OTEsImV4cCI6MjAwMjI1NTQ5MX0.hBtLKFdKk0PTl3jplGXvfCql6HPYqDSNxtELK8FAfD0"
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
 );

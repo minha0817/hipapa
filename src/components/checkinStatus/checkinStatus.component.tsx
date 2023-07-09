e14a061a692f7eb5de60d7f229c826821cf9a5fa
minha0817 <minhakk24@gmail.com>
2023-07-09 00:01:42 -0700

    Update styling for status

--- src/components/checkinStatus/checkinStatus.component.tsx
+++ src/components/checkinStatus/checkinStatus.component.tsx
@@ -2,7 +2,7 @@
 import { FC, PropsWithChildren } from "react";
 
 import styles from "./checkinStatus.styles.module.scss";
-import {PiCheckCircleBold, PiCircleBold, PiCircleDashedBold} from "react-icons/pi";
+import { PiCircleBold, PiCircleDashedBold} from "react-icons/pi";
 import {RiCheckboxBlankCircleFill} from "react-icons/ri";
 
 type CheckinStatusProps = {
@@ -12,7 +12,7 @@ type CheckinStatusProps = {
 const CheckinStatusComponent: FC<PropsWithChildren<CheckinStatusProps>> = () => {
   return (<div className={styles.checkinStatus}>
     <div className="box">
-      <RiCheckboxBlankCircleFill size={20}/> 
+      <RiCheckboxBlankCircleFill size={20} color="green"/> 
       <span>3</span>
       <PiCircleBold size={20} />
       <span>1</span>
